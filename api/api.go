75b0721f34f375fd6f65668f4cef6eb5ccb6bc03
Maxim Lebedev <git@toby3d.ru>
2018-03-30 19:39:41 +0500

    :construction: Added index and post pages routes
    close #13

--- api/api.go
+++ api/api.go
@@ -12,8 +12,14 @@ var Router = gramework.New()
 func New() {
 	Router = gramework.New()
 
+	Router.GET("/", handlerIndex)
+
+	Router.GET("/post/:id", handlerPost)
+
 	Router.GET("/editor", handlerEditor)
+
 	Router.POST("/publish", handlerPublish)
+
 }
 
 func Run() error {
